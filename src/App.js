d79d51aaad9e6cf021df8f645b1aa1b6087e1087
Rafael Ceron <rrfcero98@gmail.com>
2023-01-04 10:28:35 -0600

    Agregando logo de freecodecamp

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import freeCodeCampLogo from './img/freecodecamp-logo.png'
 
 function App() {
   return (
-    <div className="App">
+    <div className="aplicacion-tareas">
+      <div className='freecodecamp-logo-contenedor'>
+        <img 
+          src={freeCodeCampLogo} 
+          className='freecodecamp-logo'
+          alt='Logo de freecodecamp' 
+        />
+      </div>
     </div>
   );
 }
