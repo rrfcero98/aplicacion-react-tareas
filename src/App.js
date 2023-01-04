0865b71fde2fae085b2722b86d9e0ed96fd8f04e
Rafael Ceron <rrfcero98@gmail.com>
2023-01-04 14:13:41 -0600

    Agregando formulario con sus estilos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Logo from './components/Logo';
 import Tarea from './components/Tarea';
+import TareaFormulario from './components/TareaFormulario';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Logo />
       <div className='tareas-lista-principal'>
         <h1>Mis tareas</h1>
+        <TareaFormulario  />
         <Tarea texto='Esto es la primer tarea' completada={true} />
         <Tarea texto='Esto es la segunda tarea' completada={false} />
       </div>
