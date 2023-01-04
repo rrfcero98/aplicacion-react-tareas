04c819c85e09e286eb82f641f8a546b3f188b3a2
Rafael Ceron <rrfcero98@gmail.com>
2023-01-04 14:48:19 -0600

    Agregando estructura inicial de renderización de tareas en ListaDeTareas

--- src/components/ListaDeTareas.js
+++ src/components/ListaDeTareas.js
@@ -1,18 +1,29 @@
-import React from 'react';
-import TareaFormulario from './TareaFormulario';
-import '../style/ListaDeTareas.css';
+import React, { useState } from "react"; //Importando hook de otra forma
+import TareaFormulario from "./TareaFormulario";
+import Tarea from './Tarea';
+import "../style/ListaDeTareas.css";
 
-function ListaDeTareas () {
-    return(
-        //Un componente no puede tener más de un div al mismo nivel
-        //Los fragmentos <></> en react sirven solo para mantener un orden en el código y a la hora de compilar desaparacen
-        <>
-          <TareaFormulario />
-          <div className='tareas-lista-contenedor'>
-            LISTA DE TAREAS
-          </div>    
-        </>
-    );
+function ListaDeTareas() {
+
+  const [tareas, setTareas] = useState([]);
+
+  return (
+    //Un componente no puede tener más de un div al mismo nivel
+    //Los fragmentos <></> en react sirven solo para mantener un orden en el código y a la hora de compilar desaparacen
+    <>
+      <TareaFormulario />
+      <div className="tareas-lista-contenedor">
+        { //Renderizando una lista de componentes, cada tarea se mostrará como un objeto en el array
+          tareas.map((tarea) =>
+            <Tarea 
+              texto={tarea.texto}
+              completada={tarea.completada}
+            />
+          )
+        }
+      </div>
+    </>
+  );
 }
 
 export default ListaDeTareas;
